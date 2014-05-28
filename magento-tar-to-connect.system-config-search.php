a3a281e57c747719bdc7897593006b376e543a8e
Alan Storm <astorm@alanstorm.com>
2014-05-27 17:50:55 -0700

    preparing new connect extension

---
+++ magento-tar-to-connect.system-config-search.php
@@ -0,0 +1,24 @@
+<?php
+return array(
+'base_dir'           => '/fakehome/Documents/github/MagentoBetter404/var/build',
+'archive_files'      => 'System_Config_Search.tar',
+'extension_name'     => 'System_Config_Search',
+'extension_version'  => '0.7.0',
+'archive_connect'    => 'System_Config_Search-0.1.0.tgz',
+'path_output'        => '/fakehome/Pulsestorm/var/build-connect',
+
+
+'stability'          => 'stable',
+'license'            => 'MIT',
+'channel'            => 'community',
+'summary'            => 'This extension installs a search form that allows you to quickly find System Configuration fields.',
+'description'        => 'This extension installs a search form that allows you to quickly find System Configuration fields.',
+'notes'              => '',
+
+'author_name'        => 'Alan Storm',
+'author_user'        => 'alanstorm',
+'author_email'       => 'foo@example.com',
+'php_min'            => '5.2.0',
+'php_max'            => '6.0.0',
+'skip_version_compare' => false,
+);
